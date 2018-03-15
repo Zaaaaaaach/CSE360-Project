5dbea93f14d5fdf7d6a0fe04330ffb875c5b57e5
BATmanC3 <btycksen@gmail.com>
2018-03-15 00:32:14 -0700

    formatFile method works

--- CSE360Project/src/FileData.java
+++ CSE360Project/src/FileData.java
@@ -19,6 +19,7 @@ public class FileData {
 		this.avgLineWords = 0.00;
 		this.avgLineLength = 0.00;
 		this.avgFormat = new DecimalFormat("0.00");
+		formatFile(input);
 		calculateFileData(input);
 
 	}
@@ -47,6 +48,53 @@ public class FileData {
 		catch(Exception e2) {
 
 		}
+	}
+	
+	public void formatFile(String input) {
+		String line = "";
+		int count = 0;
+		try {
+			FileReader reader = new FileReader(input);
+			BufferedReader br = new BufferedReader(reader);
+			FileWriter writer = new FileWriter("output.txt");
+			PrintWriter outputFile = new PrintWriter(writer);
+			int limit = 0;
+			while ((line = br.readLine()) != null) {
+				line = line.trim();
+				if(line.length() == 0) {
+					
+				}
+				else if(limit == 0 && line.length() > 80 && line.indexOf(" ") == -1) {
+					outputFile.println(line);
+				}
+				else if(limit > 0 && line.length() > 80 && line.indexOf(" ") == -1) {
+					outputFile.println();
+					outputFile.println(line);
+					limit = 0;
+				}
+				else {
+					words = line.split(" ");
+					for(int i = 0; i < words.length; i++) {
+						if(limit == 0) {
+							outputFile.print(words[i]);
+							limit = words[i].length();
+						}
+						else if(limit+words[i].length()+1 <= 80) {
+							outputFile.print(" "+words[i]);
+							limit += words[i].length()+1;
+						}
+						else {
+							outputFile.println();
+							outputFile.print(words[i]);
+							limit = words[i].length();
+						}
+					}
+				}
+			}
+			br.close();
+			outputFile.close();
+		}
+		catch(Exception e2) {}
 	}
 	
 	public int getNumOfLines() {
